3cc299987a5bc9e32a386a468729549203be5ced
Cotue <jovenegas@estudiantec.cr>
2024-10-08 21:26:30 -0600

    DataBase

--- QueryProcessor/obj/Debug/net8.0/QueryProcessor.AssemblyInfo.cs
+++ QueryProcessor/obj/Debug/net8.0/QueryProcessor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueryProcessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41163f50adf77cb404b3d2f7dc6a966ec6828c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a564ea5a3fd7ffb1aea8150432e970666d954db")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueryProcessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QueryProcessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
